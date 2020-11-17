55134970057d796ebdbfcf228054227dd2a52e1d
Ying Fu <YFu3@slb.com>
2020-11-17 15:15:51 +0800

    feat: add input validation ACtest2

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,9 +1,22 @@
 export class MultiplicationTable {
-  private isValidRange(start: number, end: number): boolean {
-    return start <= end
+  private isStartGreaterThanEnd(start: number, end: number): boolean {
+    return start > end
   }
+  private isInRange(number: number) {
+    const startNumber = 1
+    const endNumber = 10
+    if(number >= startNumber && number <= endNumber) return true
+    return false
+  }
+  private isValidRange(start: number, end: number): boolean {
+    return this.isInRange(start) && this.isInRange(end)
+  }
+
   public render(start: number, end: number): string {
-    if(!this.isValidRange(start, end))  return ''
+    if(this.isStartGreaterThanEnd(start, end))  return ''
+    if(!this.isValidRange(start, end)) return 'The input number over the range scope [1, 10]'
     return '1*1=1'
   }
+
+
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -14,17 +14,30 @@ describe('Multiplication Table', () => {
     expect(rendered).toBe('1*1=1')
   })
 
-  it('should render return nothing when multiplication table of (1, 1)', () => {
+  // it('should render return nothing when multiplication table of (1, 1)', () => {
+  //   // given
+  //   const table = new MultiplicationTable()
+  //   const start = 10
+  //   const end = 9
+
+  //   //when
+  //   const rendered = table.render(start, end)
+
+  //   //then
+  //   expect(rendered).toBe('')
+  // })
+
+  it('should render in range [1, 10] when multiplication table of (1, 1)', () => {
     // given
     const table = new MultiplicationTable()
-    const start = 10
-    const end = 9
+    const start = 2
+    const end = 11
 
     //when
     const rendered = table.render(start, end)
 
     //then
-    expect(rendered).toBe('')
+    expect(rendered).toBe('The input number over the range scope [1, 10]')
   })
 
 })
